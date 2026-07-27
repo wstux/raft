37d0ede71eb0c197d0eb7f2a6b55a0c765741edf
Chistyakov Alexander <wstux1@gmail.com>
2026-07-27 18:32:44 +0300

    + libs/raft_le: Updated raft server;

--- src/libs/raft_le/details/server.cpp
+++ src/libs/raft_le/details/server.cpp
@@ -25,7 +25,9 @@
 #include <cassert>
 
 #include "raft_le/server.h"
+#include "raft_le/details/context.h"
 #include "raft_le/details/logger_channels.h"
+#include "raft_le/details/logging.h"
 
 namespace wstux {
 namespace raft {
@@ -44,26 +46,39 @@ server::server(const server_id_t id, const io::ptr& /*p_io*/, const ilogger_fact
 
 cluster_config_t server::cluster_cfg() const
 {
-    return {};
+    return details::utils::wrap(m_p_ctx, &details::utils::make_config);
 }
 
 void server::deinit()
-{}
+{
+    context_ptr p_ctx = m_p_ctx;
+    p_ctx->p_io->deinit();
+}
 
 const std::string& server::endpoint() const
 {
-    static std::string ep;
-    return ep;
+    context_ptr p_ctx = m_p_ctx;
+    return p_ctx->config.endpoint;
 }
 
 bool server::init()
 {
-    return false;
+    context_ptr p_ctx = m_p_ctx;
+    const bool is_inited = details::sturtup::init(*p_ctx, m_id);
+    if (! is_inited) {
+        RAFT_ROOT_LOG_ERROR((*p_ctx), "Filed to init raft server.");
+        return false;
+    }
+    //m_p_ctx->election_task = ;
+    //m_p_ctx->heartbeat_task = ;
+
+    return true;
 }
 
 bool server::is_inited() const
 {
-    return false;
+    context_ptr p_ctx = m_p_ctx;
+    return (p_ctx->election_task.get() != nullptr);
 }
 
 bool server::is_leader() const
@@ -78,6 +93,17 @@ void server::handle_message(const buffer_type& /*msg_buf*/)
     }
 }
 
+bool server::load(details::context& ctx)
+{
+    const bool is_loaded = details::sturtup::load(ctx);
+    if (! is_loaded) {
+        RAFT_ROOT_LOG_ERROR(ctx, "Failed to load raft configuration.");
+        return false;
+    }
+
+    return true;
+}
+
 std::vector<std::string> server::logging_channels()
 {
     return details::loggers::logging_channels();
@@ -85,7 +111,9 @@ std::vector<std::string> server::logging_channels()
 
 bool server::start()
 {
+    context_ptr p_ctx = m_p_ctx;
     if (! m_is_stop.exchange(false)) {
+        RAFT_ROOT_LOG_WARN((*p_ctx), "Raft server " << p_ctx->id << " has been already started.");
         return false;
     }
 
@@ -93,11 +121,26 @@ bool server::start()
         return false;
     }
 
+    if (! load(*p_ctx)) {
+        return false;
+    }
+
+    RAFT_ROOT_LOG_INFO((*p_ctx), "Starting raft server " << p_ctx->id << ".");
+
     return true;
 }
 
 void server::stop()
-{}
+{
+    context_ptr p_ctx = m_p_ctx;
+    if (m_is_stop.exchange(true)) {
+        RAFT_ROOT_LOG_WARN((*p_ctx), "Raft server " << p_ctx->id << " has been already stopped.");
+        return;
+    }
+    RAFT_ROOT_LOG_INFO((*p_ctx), "Stopping raft server " << p_ctx->id << ".");
+
+    p_ctx->p_scheduler->stop();
+}
 
 } // namespace le
 } // namespace raft
