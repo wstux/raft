37716780a0393174f6eb46515a6ac849eea7e34a
Chistyakov Alexander <wstux1@gmail.com>
2026-09-06 10:24:43 +0300

    + libs/raft: Refactored async membership process;

--- src/libs/raft/details/replication/membership.cpp
+++ src/libs/raft/details/replication/membership.cpp
@@ -38,35 +38,35 @@ namespace replication {
 namespace membership {
 namespace {
 
+void async_io_cb(context& ctx, bool accept, entries::async::apply_context::ptr p_async_ctx)
+{
+    assert(ctx.state.tasks_in_process > 0);
+    --ctx.state.tasks_in_process;
+    entries::apply_callback(ctx, accept, p_async_ctx->index, p_async_ctx->entries);
+}
+
 bool change_configuration(context& ctx, cluster_config& cluster_cfg)
 {
     const index_t index = ctx.log.last_index() + 1;
 
     entries::async::apply_context::ptr p_async_ctx;
     const bool accept = entries::apply_configuration(ctx, std::move(cluster_cfg), p_async_ctx);
-    if (accept && ctx.is_async_io) {
-        assert(p_async_ctx);
-
-        scheduler::handler_type handler_fn = [&ctx, p_async_ctx = std::move(p_async_ctx)] () -> void {
-            RAFT_LOG_TRACE(ctx, "Server %llu(%s) is changing new peer asynchronously.", ctx.id, ctx.role.str());
-            // Perform blocking disk write outside the critical section (mutex)
-            const bool accept = ctx.p_io->append(p_async_ctx->entries);
-            ctx.schd.execute_strand([&ctx, accept, p_async_ctx = std::move(p_async_ctx)] () -> void {
-                assert(ctx.state.tasks_in_process > 0);
-                --ctx.state.tasks_in_process;
-                entries::apply_callback(ctx, accept, p_async_ctx->index, p_async_ctx->entries);
-            });
-        };
-
-        ++ctx.state.tasks_in_process;
-        ctx.schd.execute_async(std::move(handler_fn));
-    }
-
     if (accept) {
+        if (ctx.is_async_io && p_async_ctx) {
+            scheduler::handler_type handler_fn = [&ctx, p_async_ctx = std::move(p_async_ctx)] () -> void {
+                RAFT_LOG_TRACE(ctx, "Server %llu(%s) is changing new peer asynchronously.", ctx.id, ctx.role.str());
+                const bool accept = ctx.p_io->append(p_async_ctx->entries);
+                ctx.schd.execute_strand([&ctx, accept, p_async_ctx = std::move(p_async_ctx)] () -> void {
+                    async_io_cb(ctx, accept, p_async_ctx);
+                });
+            };
+
+            ++ctx.state.tasks_in_process;
+            ctx.schd.execute_async(std::move(handler_fn));
+        }
         append_entries::request(ctx);
         ctx.state.configuration_uncommitted_index = index;
     }
-
     return accept;
 }
 
